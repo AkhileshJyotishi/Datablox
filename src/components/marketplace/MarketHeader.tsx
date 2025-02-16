c6639db907fe935b7c76a1a4422246aa73ebd221
DISHA DWIVEDI <dishadwivedi123g@gmail.com>
2025-02-16 17:25:09 +0530

    connected

--- src/components/marketplace/MarketHeader.tsx
+++ src/components/marketplace/MarketHeader.tsx
@@ -1,8 +1,14 @@
+'use client';
 import Link from 'next/link'
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
-
+import { KiiChain } from "@/kiichain";
+import { ConnectKitButton } from "connectkit";
+import {useConnect, useAccount} from 'wagmi'
 export default function MarketHeader() {
+
+    const {address,isConnected} = useAccount();
+
     return (
         <div className='flex justify-between mx-10 py-10 items-center'>
             <div className='flex gap-8 font-bold text-gray-400 uppercase text-md'>
@@ -15,9 +21,24 @@ export default function MarketHeader() {
                     <input type="text" className='bg-transparent outline-none w-[300px]' placeholder='Search...' />
                     <CiSearch size={20} />
                 </div>
-                <button className='text-[#ff4092] font-bold border border-[#303030] bg-[#141414] px-4 text-sm'>
-                    Connect Wallet
-                </button>
+               { !isConnected ?  <ConnectKitButton.Custom>
+  {({ show }) => (
+    <button
+      className="text-[#ff4092] font-bold border border-[#303030] bg-[#141414] px-4 text-sm"
+      onClick={show}
+    >
+      Connect Wallet
+    </button>
+  )}
+</ConnectKitButton.Custom> : 
+<button
+      className="text-[#ff4092] font-bold border border-[#303030] bg-[#141414] px-4 text-sm"
+    >
+      {address && <span>{address.slice(0, 4)}...{address.slice(-4)}</span>}
+    </button>
+}
+
+                
             </div>
 
         </div>
