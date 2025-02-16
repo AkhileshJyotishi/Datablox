e93cd03daf258dbdaba6db8be4708660cc640015
Ankit Yadav <mahesh9426346889@gmail.com>
2025-02-17 02:44:55 +0530

    Add animation background in marketplace

--- src/components/marketplace/Marketplace.tsx
+++ src/components/marketplace/Marketplace.tsx
@@ -7,19 +7,23 @@ import RecentlyPublished from './RecentlyPublished'
 import TopTagsBySales from './TopTagsBySales'
 import TopPublishers from './TopPublishers'
 import AllData from './AllDatasets'
+import { BackgroundCellCore } from '../misc/BackgroundCellAnimation' 
 
 export default function Marketplace() {
   return (
-    <div className='bg-[#0a0a0a] mb-20'>
-      <MarketHeader />
-      <Markethero />
-      <div className='px-[10%] flex flex-col gap-10'>
-        <YourBookMark />
-        <MostSales />
-        <TopPublishers />
-        <TopTagsBySales />
-        <RecentlyPublished />
-        <AllData/>
+    <div className='bg-[#0a0a0a] my-4 relative'>
+      <div className="relative flex justify-center overflow-hidden bg-[#0a0a0a] ">
+        <BackgroundCellCore />
+        <div className="pointer-events-none relative z-50 mt-16 mb-3 select-none">
+          <Markethero />
+          <div className='px-[10%] flex flex-col gap-10'>
+            <YourBookMark />
+            <TopPublishers />
+            <TopTagsBySales />
+            <RecentlyPublished />
+            <AllData/>
+          </div>
+        </div>
       </div>
     </div>
   )
