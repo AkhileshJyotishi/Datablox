d4746626717da897dc09ebb0a8455421b61f09c7
Ankit Yadav <mahesh9426346889@gmail.com>
2025-02-18 01:21:21 +0530

    Some ui updates

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -3,7 +3,7 @@ import DataPacketSkeleton from './skeleton'
 
 const loading = () => {
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-6 mt-12 xl:border rounded-md dark:border-neutral-800'>
+    <div className='grid grid-cols-1 lg:grid-cols-6 mt-12 gap-4 rounded-md '>
         {
             Array.from({length: 10}, (_, index) => (
                 <DataPacketSkeleton key={index}/>
