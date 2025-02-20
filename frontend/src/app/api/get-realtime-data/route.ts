9996353e3628e5e90ec3ac04717b1cf7a9c24e64
Umesh Singh Verma <119877487+umeshSinghVerma@users.noreply.github.com>
2025-02-20 22:38:15 +0530

    updates

--- frontend/src/app/api/get-realtime-data/route.ts
+++ frontend/src/app/api/get-realtime-data/route.ts
@@ -1,11 +1,18 @@
+import data from "@/data/utils";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
     try {
         const { searchParams } = new URL(request.url);
-        const user = searchParams.get("user");
         const id = searchParams.get("id");
-        return NextResponse.json("hello", { status: 200 });
+
+        const numericId = id ? Number(id) : NaN;
+
+        if (!isNaN(numericId) && (data as Record<number, any>)[numericId]) {
+            return NextResponse.json((data as Record<number, any>)[numericId].data, { status: 200 });
+        } else {
+            return NextResponse.json("No dataset with this id", { status: 404 });
+        }
     } catch (error) {
         return NextResponse.json({ error: (error as Error).message }, { status: 500 });
     }
