54988567d64547f5248399630ee334d705b90583
Umesh Singh Verma <119877487+umeshSinghVerma@users.noreply.github.com>
2025-02-21 10:17:10 +0530

    final commit

--- frontend/src/app/api/get-realtime-data/route.ts
+++ frontend/src/app/api/get-realtime-data/route.ts
@@ -5,10 +5,11 @@ export async function GET(request: Request) {
     try {
         const { searchParams } = new URL(request.url);
         const id = searchParams.get("id");
-        if (Number(id)) {
+        const user = searchParams.get("user");
+        if (Number(id) >= 0 && !isNaN(Number(id)) && user) {
             return NextResponse.json(data[Number(id)], { status: 200 });
         } else {
-            return NextResponse.json("No dataset with this id", { status: 404 });
+            return NextResponse.json("No dataset with this id or no dataset is alloted to this user", { status: 404 });
         }
     } catch (error) {
         return NextResponse.json({ error: (error as Error).message }, { status: 500 });
--- frontend/src/app/realtime/[id]/page.tsx
+++ frontend/src/app/realtime/[id]/page.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import CompleteMetaData from "@/components/dataset-page/CompleteMetaData"
-import NoDataFound from "../../[datasetId]/noDataFound"
 import realtime from "@/constants/realtime"
 
 export default async function Page({ params }: { params: { id: string } }) {
