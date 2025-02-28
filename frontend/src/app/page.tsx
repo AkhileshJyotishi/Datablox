cb215c317c3e977cdab3d338bf2b7b1ee312f3c9
Ankit Yadav <mahesh9426346889@gmail.com>
2025-03-01 03:13:30 +0530

    Fixes and UI changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,10 +16,6 @@ export const metadata: Metadata = {
   description: "Home page",
 }
 
-const Skeleton = () => (
-  <div className="flex h-full min-h-[6rem] w-full flex-1 rounded-xl bg-gradient-to-br from-neutral-200 to-neutral-100 dark:from-neutral-900 dark:to-neutral-800"></div>
-)
-
 const HomePage: NextPage = () => {
   return (
     <>
